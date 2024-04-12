6a1c3765ada81ec5bbdace643f350f14df228f0f
Ron Valdman <ronvaldman@gmail.com>
2024-04-13 00:40:26 +0300

    13/04/24
    13/04/24

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import socket
 import json
 import os
 import requests
+import random
 
 YELLOW = '\033[93m'
 GREEN = '\033[92m'
@@ -11,11 +12,23 @@ RED = '\033[91m'
 TRIVIA_FILE = 'trivia_data.json'
 HTML_FILE = 'index.html'
 CSS_FILE = None
+JSON_DATA_FILE = 'trivia_data.json'
 
 with open(TRIVIA_FILE, 'r') as file:
     trivia_data = json.load(file)
 
 app = Flask(__name__, template_folder='templates')
+
+
+def load_json_data():
+    with open(JSON_DATA_FILE, 'r') as file:
+        return json.load(file)
+
+
+def save_json_data(data):
+    with open(JSON_DATA_FILE, 'w') as file:
+        json.dump(data, file, indent=4)
+
 
 @app.route('/')
 def welcome():
@@ -29,12 +42,11 @@ def get_all_trivia_questions():
 
 @app.route('/trivia/delete/<int:index>', methods=['DELETE'])
 def delete_trivia_question(index):
-    global trivia_data
+    trivia_data = load_json_data()
 
-    if 0 <= index < len(trivia_data['results']):
-        del trivia_data['results'][index]
-        with open('trivia_data.json', 'w') as file:
-            json.dump(trivia_data, file, indent=4)
+    if 0 <= index < len(trivia_data):
+        del trivia_data[index]
+        save_json_data(trivia_data)
         return jsonify({'message': 'Trivia question deleted successfully'})
     else:
         return jsonify({'error': 'Invalid index'})
@@ -45,13 +57,21 @@ def add_trivia_question():
     global trivia_data
     if request.method == 'POST':
         new_question = request.json
+        if isinstance(trivia_data, dict):
+            trivia_data = [trivia_data]
         trivia_data.append(new_question)
         with open('trivia_data.json', 'w') as file:
             json.dump(trivia_data, file, indent=4)
-
         return jsonify({'message': 'Trivia question added successfully'})
     else:
         return jsonify({'error': 'Only POST requests are allowed for this endpoint'})
+
+
+@app.route('/trivia/random', methods=['GET'])
+def get_random_question():
+    trivia_data = load_json_data()
+    random_question = random.choice(trivia_data)
+    return jsonify(random_question)
 
 
 def is_port_in_use(port):
@@ -73,4 +93,3 @@ if __name__ == '__main__':
         #     port = 5000
         else:
             print(RED + "Port {} is already in use. Please select a different port.".format(port))
-
